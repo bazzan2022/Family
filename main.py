0995423cd0206d9953176053ebc8bd3d7a2d62bb
besh2021 <80162178+besh2021@users.noreply.github.com>
2021-10-22 06:21:18 +0300

    first commit

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import pixellib
 from pixellib.torchbackend.instance import instanceSegmentation
 import cv2
-import numpy as np
+import numpy as np #machine learning library to deal with huge data
 import torch
 from torch.autograd import Variable
 
-from net import Net
+from net import Net #neural network
 from utils import StyleLoader
 
 
@@ -21,15 +21,17 @@ def main():
     ins = instanceSegmentation()
     ins.load_model("pointrend_resnet50.pkl")
 
-    # Load replacement background image
-    background_image = cv2.imread("background.jpg")
+    # Load foreground image
+    background_image = cv2.imread("flower.jpg")
     background_image = background_image.astype("uint8")
-
+    # Load background image
+    background_imagee = cv2.imread("background.jpg")
+    background_imagee = background_imagee.astype("uint8")
     # Select the resnet50 classes you want to segment by
     target_classes = ins.select_target_classes(person=True)
 
     # Initialize NST
-    style_model = Net(ngf=128)
+    style_model = Net(ngf=128)#next cell
     model_dict = torch.load("models/21styles.model")
     model_dict_clone = model_dict.copy()
     for key, value in model_dict_clone.items():
@@ -56,27 +58,27 @@ def main():
 
         mask.resize((mask.shape[0], mask.shape[1], 1), refcheck=False)
 
-        mask_inverse = 1 - mask
-        mask_inverse = mask_inverse.astype("uint8")
+        mask_inverse = mask
 
         # Composite background with foreground using mask
         bg = cv2.bitwise_and(background_image,
                              background_image,
                              mask=mask_inverse)
+        bgr = cv2.bitwise_and(background_imagee,
+                         background_imagee,
+                         mask=1-mask_inverse)
         fg = cv2.bitwise_and(frame, frame, mask=mask)
-
-        composite = cv2.add(bg, fg)
+        composite = cv2.add(bg, bgr, fg)
 
         # Prepare for NST
         nst_image = np.array(composite).transpose(2, 0, 1)
-
         # Load NST Style
         style_v = style_loader.get(style_idx)
         style_v = Variable(style_v.data)
         style_model.setTarget(style_v)
 
         # Apply style to image
-        nst_image = torch.from_numpy(nst_image).unsqueeze(0).float()
+        nst_image = torch.Tensor(nst_image).unsqueeze(0).float()
         nst_image = Variable(nst_image)
         nst_image = style_model(nst_image)
 
